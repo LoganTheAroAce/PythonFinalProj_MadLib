ad54e164c4a603c814a5ea01a90546b873f8b180
purpl <purpl@LOGANSPC>
2025-02-27 09:15:19 -0330

    Add project files.

---
+++ PythonFinalProj_MadLib.py
@@ -0,0 +1,18 @@
+#Logan Parrell :3
+#Mad-Lib Generator
+
+#The Words that get inserted into the story
+noun1 = input("Choose A Noun. (Ex. Monkey, Horse, Human)")
+noun2 = input("Choose A Noun. (Ex. Cage, House, Gaming Setup)")
+noun3 = input("Choose A Noun. (Ex. Cat, Elephant, Rock)")
+verb1 = input("Choose A (Past Tense) Verb. (Ex. Ran, Jumped, Walked)")
+adverb1 = input("Choose An Adverb. (Ex. Well, Badly, Quickly)")
+adject1 = input("Choose An Adjective. (Ex. Playful, Cool, Massive)")
+if adject1 == "Massive":
+        print("you know what else is massive?")
+        #Easter Egg :3
+adject2 = input("Choose An Adjective. (Ex. Playful, Cool, Large)")
+adject3 = input("Choose An Adjective. (Ex. Tasty, Gross, Tiny)")
+
+#The Story
+print("Today I went to the zoo. I saw a", adject1, noun1, "jumping up and down in its tree. They", verb1, adverb1, "through the large tunnle that led to its", adject2, noun2, ". I got some peanuts and passed them through the cage to a gigantic gray", noun3, "towering above my head. Feeding that animal made me hungry. I went to get a", adject3, "scoop of ice cream.")
